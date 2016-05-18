9d8da6ce6021cfefe95a29c6d72e48ee71499e3f
daisuke shinmachi <sinmachi@soka.ac.jp>
2016-05-18 18:07:22 +0900

    upload stanza : iupac

---
+++ glytoucan_stanza/entry_provider/iupac/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Handlebars.registerHelper('iupacNothingFound', function (data) {
+  return data.length === 0 ? '<p class="stanzaNothingFound">Can\'t get IUPAC data.</p>' : '';
+});
+
+Stanza(function (stanza, params) {
+  var q = stanza.query({
+    endpoint: "http://test.ts.glytoucan.org/sparql",
+    template: "stanza.rq",
+    parameters: {
+      accessionNumber: params.acc
+    }
+  });
+  q.done(function (data) {
+    stanza.render({
+      template: "stanza.html",
+      parameters: {
+        data: data.results.bindings
+      },
+    });
+  });
+  q.fail(function (jqXHR) {
+    console.log(jqXHR);
+  });
+});
