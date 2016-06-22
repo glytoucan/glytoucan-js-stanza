07c3bfeb3b2302abf83543a4bca7b5c6124b1fcf
daisuke shinmachi <sinmachi@soka.ac.jp>
2016-06-22 18:31:25 +0900

    modified external stanza for display each database entry labels

--- glytoucan_stanza/entry_provider/external/index.js
+++ glytoucan_stanza/entry_provider/external/index.js
@@ -16,14 +16,14 @@ Stanza(function (stanza, params) {
     var categoryExternal = {};
     var list, label, id, url;
     data.results.bindings.forEach(function (d) {
-      categoryExternal[d.from.value] = categoryExternal[d.from.value] || {list: [], label: ''};
-      label = d.db_label ? d.db_label.value : undefined;
+      categoryExternal[d.entry_label.value] = categoryExternal[d.entry_label.value] || {list: [], label: ''};
+      label = d.entry_label ? d.entry_label.value : undefined;
       id = d.external_id ? d.external_id.value : undefined;
       url = d.url ? d.url.value : undefined;
       from = d.from ? d.from.value : undefined;
-      categoryExternal[d.from.value].label = categoryExternal[d.from.value].label || label;
-      categoryExternal[d.from.value].from = categoryExternal[d.from.value].from || from;
-      categoryExternal[d.from.value].list.push({id: id, url: url});
+      categoryExternal[d.entry_label.value].label = categoryExternal[d.entry_label.value].label || label;
+      categoryExternal[d.entry_label.value].from = categoryExternal[d.entry_label.value].from || from;
+      categoryExternal[d.entry_label.value].list.push({id: id, url: url});
     });
     stanza.render({
       template: "stanza.html",
