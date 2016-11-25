2c51143b69b20bf3e0707284ef08a90b76ba640f
daisuke shinmachi <sinmachi@soka.ac.jp>
2016-11-25 16:45:58 +0900

    add "From" field has link to explanation of partner to literature stanza

--- glytoucan_stanza/entry_provider/literature/index.js
+++ glytoucan_stanza/entry_provider/literature/index.js
@@ -25,11 +25,15 @@ Stanza(function (stanza, params) {
   });
   q.done(function (data) {
     var categoryPubMed = {};
-    var id, url;
+    var id, url, from, description, partnerurl;
     data.results.bindings.forEach(function (d) {
       categoryPubMed[d.from.value] = categoryPubMed[d.from.value] || [];
       id = d.pubmed_id ? d.pubmed_id.value : undefined;
       url = d.pubmed_url ? d.pubmed_url.value : undefined;
+      description = d.description ? d.description.value : undefined;
+      partnerurl = d.partner_url ? d.partner_url.value : undefined;
+      categoryPubMed[d.from.value].description = categoryPubMed[d.from.value].description || description;
+      categoryPubMed[d.from.value].partnerurl= categoryPubMed[d.from.value].partnerurl || partnerurl;
       categoryPubMed[d.from.value].push({id: id, url: url});
     });
     stanza.render({
@@ -38,6 +42,11 @@ Stanza(function (stanza, params) {
         data: categoryPubMed
       },
     });
+    Array.prototype.forEach.call(stanza.selectAll('.source_btn'), function (el) {
+      el.addEventListener('click', function (e) {
+        e.currentTarget.nextElementSibling.classList.toggle('source_content--show');
+      });
+    }); 
   });
   q.fail(function (jqXHR) {
     console.log(jqXHR);
